a84da8a427ef12cf014964adb18645269d5f1acf
MrinalBhoumick <mrinalbhoumick0610@gmail.com>
2024-04-07 13:43:39 +0530

    updated request mapping template

--- scripts/update-request-mapping-template.sh
+++ scripts/update-request-mapping-template.sh
@@ -1,25 +1,19 @@
 #!/bin/bash
-REQUEST_TEMPLATE_FILE="request.vtl"
 
 # List all types from the schema
 TYPES=$(aws appsync list-types --api-id "$API_ID")
 
-# Extract type names
-TYPE_NAMES=$(echo "$TYPES" | jq -r '.types[] | select(.definition | startswith("type")) | .name')
+# Extract resolver names and type names
+RESOLVER_NAMES=$(echo "$TYPES" | jq -r '.types[] | select(.definition | contains("type ResolverType")) | .name')
+TYPE_NAMES=$(echo "$TYPES" | jq -r '.types[] | select(.definition | contains("type ResolverType")) | .name')
 
-# Loop over type names and update request mapping template for each resolver
-for TYPE_NAME in $TYPE_NAMES; do
-  RESOLVERS=$(aws appsync list-resolvers --api-id "$API_ID" --type-name "$TYPE_NAME")
-  RESOLVER_NAMES=$(echo "$RESOLVERS" | jq -r '.resolvers[] | .fieldName')
-  
-  # Loop over resolver names and update request mapping template
-  while read -r RESOLVER_NAME; do
-    if aws appsync update-resolver --api-id "$API_ID" --type-name "$TYPE_NAME" --field-name "$RESOLVER_NAME" --request-mapping-template file://"$REQUEST_TEMPLATE_FILE"; then
-      echo "Updated request mapping template for resolver $RESOLVER_NAME of type $TYPE_NAME."
-    else
-      echo "Failed to update request mapping template for resolver $RESOLVER_NAME of type $TYPE_NAME."
-    fi
-  done <<< "$RESOLVER_NAMES"
-done
+# Loop over resolver names and update request mapping template
+while IFS= read -r RESOLVER_NAME && IFS= read -r TYPE_NAME; do
+  if aws appsync update-resolver --api-id "$API_ID" --type-name "$TYPE_NAME" --field-name "$RESOLVER_NAME" --request-mapping-template file://"$REQUEST_TEMPLATE_FILE"; then
+    echo "Updated request mapping template for resolver $RESOLVER_NAME of type $TYPE_NAME."
+  else
+    echo "Failed to update request mapping template for resolver $RESOLVER_NAME of type $TYPE_NAME."
+  fi
+done < <(paste <(printf "%s\n" "$RESOLVER_NAMES") <(printf "%s\n" "$TYPE_NAMES"))
 
 echo "Completed updating request mapping templates."
