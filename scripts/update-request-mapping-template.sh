867868071fefdd1143679a4fc0b1cce014696302
MrinalBhoumick <mrinalbhoumick0610@gmail.com>
2024-04-07 13:50:48 +0530

    updated request mapping template

--- scripts/update-request-mapping-template.sh
+++ scripts/update-request-mapping-template.sh
@@ -3,18 +3,15 @@
 # List all types from the schema
 TYPES=$(aws appsync list-types --api-id "$API_ID")
 
-# Extract type names
-TYPE_NAMES=$(echo "$TYPES" | jq -r '.types[].name')
+# Extract resolver type names
+RESOLVER_TYPE_NAMES=$(echo "$TYPES" | jq -r '.types[] | select(.definition | test("type ResolverType")) | .name')
 
-# Loop over type names and update request mapping template
-for TYPE_NAME in $TYPE_NAMES; do
-  # Check if the type name is a resolver type
-  if [[ "$TYPE_NAME" == *"ResolverType"* ]]; then
-    if aws appsync update-resolver --api-id "$API_ID" --type-name "$TYPE_NAME" --field-name "$RESOLVER_NAME" --request-mapping-template file://"$REQUEST_TEMPLATE_FILE"; then
-      echo "Updated request mapping template for resolver $RESOLVER_NAME of type $TYPE_NAME."
-    else
-      echo "Failed to update request mapping template for resolver $RESOLVER_NAME of type $TYPE_NAME."
-    fi
+# Loop over resolver type names and update request mapping template
+for TYPE_NAME in $RESOLVER_TYPE_NAMES; do
+  if aws appsync update-resolver --api-id "$API_ID" --type-name "$TYPE_NAME" --field-name "$RESOLVER_NAME" --request-mapping-template file://"$REQUEST_TEMPLATE_FILE"; then
+    echo "Updated request mapping template for resolver type $TYPE_NAME."
+  else
+    echo "Failed to update request mapping template for resolver type $TYPE_NAME."
   fi
 done
 
