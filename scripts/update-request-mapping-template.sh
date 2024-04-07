6eda2a9c9af3788e9914050f6b9df88b7386c3aa
MrinalBhoumick <mrinalbhoumick0610@gmail.com>
2024-04-07 14:47:38 +0530

    updated request mapping template

--- scripts/update-request-mapping-template.sh
+++ scripts/update-request-mapping-template.sh
@@ -12,7 +12,7 @@ RESOLVER_NAMES=$(echo "$RESOLVERS" | jq -r '.resolvers[].fieldName')
 
 # Loop over resolver names and update request mapping template
 for RESOLVER_NAME in $RESOLVER_NAMES; do
-  if aws appsync update-resolver --api-id "$API_ID" --type-name "$RESOLVER_TYPE_NAME" --field-name "$RESOLVER_NAME" --request-mapping-template file://"$REQUEST_TEMPLATE_FILE" --response-mapping-template "file://$RESPONSE_TEMPLATE_FILE"; then
+  if aws appsync update-resolver --api-id "$API_ID" --type-name "$RESOLVER_TYPE_NAME" --field-name "$RESOLVER_NAME" --request-mapping-template "file://$REQUEST_TEMPLATE_FILE"; then
     echo "Updated request mapping template for resolver $RESOLVER_NAME of type $RESOLVER_TYPE_NAME."
   else
     echo "Failed to update request mapping template for resolver $RESOLVER_NAME of type $RESOLVER_TYPE_NAME."
