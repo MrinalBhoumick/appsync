4e05500f608ebe26d17a1e6e6fefe7c4696356f0
MrinalBhoumick <mrinalbhoumick0610@gmail.com>
2024-04-07 15:02:33 +0530

    updated response mapping template

--- scripts/update-response-mapping-template.sh
+++ scripts/update-response-mapping-template.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
+
 RESPONSE_TEMPLATE_FILE="response.vtl"
 # List all resolvers
-
-RESOLVERS=$(aws appsync list-resolvers --api-id "$API_ID"
+RESOLVERS=$(aws appsync list-resolvers --api-id "$API_ID")
 # Extract resolver names and type names
 RESOLVER_NAMES=$(echo "$RESOLVERS" | jq -r '.resolvers[] | .fieldName')
 TYPE_NAMES=$(echo "$RESOLVERS" | jq -r '.resolvers[] | .typeName')
 
 # Loop over resolver names and update response mapping template
-while read -r RESOLVER_NAME && read -r TYPE_NAME <&3; do
-  if aws appsync update-resolver --api-id "$API_ID" --type-name "$TYPE_NAME" --field-name "$RESOLVER_NAME" --response-mapping-template file://"$RESPONSE_TEMPLATE_FILE"; then
+while read -r RESOLVER_NAME && read -r TYPE_NAME; do
+  if aws appsync update-resolver --api-id "$API_ID" --type-name "$TYPE_NAME" --field-name "$RESOLVER_NAME" --response-mapping-template "file://$RESPONSE_TEMPLATE_FILE"; then
     echo "Updated response mapping template for resolver $RESOLVER_NAME of type $TYPE_NAME."
   else
     echo "Failed to update response mapping template for resolver $RESOLVER_NAME of type $TYPE_NAME."
