37d19c21d58d04d8c7952b1f97273c47cb9144b6
MrinalBhoumick <mrinalbhoumick0610@gmail.com>
2024-04-07 15:31:58 +0530

    updated response mapping template

--- scripts/update-response-mapping-template.sh
+++ scripts/update-response-mapping-template.sh
@@ -16,16 +16,13 @@ SCHEMA=$(aws appsync get-introspection-schema --api-id "$API_ID" --format SDL)
 RESOLVER_TYPE_NAME=$(echo "$SCHEMA" | grep -oP 'type \K\w+' | grep ResolverType)
 
 # List all resolvers of the resolver type
-RESOLVERS=$(aws appsync list-resolvers --api-id "$API_ID" --type-name "$RESOLVER_TYPE_NAME")
-RESOLVER_NAMES=$(echo "$RESOLVERS" | jq -r '.resolvers[].fieldName')
-
-# Loop over resolver names and update response mapping template
-for RESOLVER_NAME in $RESOLVER_NAMES; do
-  if aws appsync update-resolver --api-id "$API_ID" --type-name "$RESOLVER_TYPE_NAME" --field-name "$RESOLVER_NAME" --response-mapping-template "file://$RESPONSE_TEMPLATE_FILE"; then
-    echo "Updated response mapping template for resolver $RESOLVER_NAME of type $RESOLVER_TYPE_NAME."
+RESOLVERS=$(aws appsync list-resolvers --api-id "$API_ID" --type-name "$RESOLVER_TYPE_NAME" --query "resolvers[*].[fieldName,typeName]" --output text)
+while IFS=$'\t' read -r RESOLVER_NAME TYPE_NAME; do
+  if aws appsync update-resolver --api-id "$API_ID" --type-name "$TYPE_NAME" --field-name "$RESOLVER_NAME" --response-mapping-template "file://$RESPONSE_TEMPLATE_FILE"; then
+    echo "Updated response mapping template for resolver $RESOLVER_NAME of type $TYPE_NAME."
   else
-    echo "Failed to update response mapping template for resolver $RESOLVER_NAME of type $RESOLVER_TYPE_NAME."
+    echo "Failed to update response mapping template for resolver $RESOLVER_NAME of type $TYPE_NAME."
   fi
-done
+done <<< "$RESOLVERS"
 
 echo "Completed updating response mapping templates."
