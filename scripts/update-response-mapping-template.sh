4cee7306ba36da11eefc580aa663e36ec9067980
MrinalBhoumick <mrinalbhoumick0610@gmail.com>
2024-04-07 15:12:22 +0530

    updated response mapping template

--- scripts/update-response-mapping-template.sh
+++ scripts/update-response-mapping-template.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Create response.vtl file with the response mapping template content
+echo '#if( $context.result && $context.result.error )
+    $util.error($context.result.error.message)
+#else
+    $util.toJson($context.result)
+#end' > response.vtl
+
 RESPONSE_TEMPLATE_FILE="response.vtl"
 # List all resolvers
 RESOLVERS=$(aws appsync list-resolvers --api-id "$API_ID")
@@ -8,8 +15,8 @@ RESOLVER_NAMES=$(echo "$RESOLVERS" | jq -r '.resolvers[] | .fieldName')
 TYPE_NAMES=$(echo "$RESOLVERS" | jq -r '.resolvers[] | .typeName')
 
 # Loop over resolver names and update response mapping template
-while read -r RESOLVER_NAME && read -r TYPE_NAME; do
-  if aws appsync update-resolver --api-id "$API_ID" --type-name "$TYPE_NAME" --field-name "$RESOLVER_NAME" --response-mapping-template "file://$RESPONSE_TEMPLATE_FILE"; then
+while read -r RESOLVER_NAME && read -r TYPE_NAME <&3; do
+  if aws appsync update-resolver --api-id "$API_ID" --type-name "$TYPE_NAME" --field-name "$RESOLVER_NAME" --response-mapping-template file://"$RESPONSE_TEMPLATE_FILE"; then
     echo "Updated response mapping template for resolver $RESOLVER_NAME of type $TYPE_NAME."
   else
     echo "Failed to update response mapping template for resolver $RESOLVER_NAME of type $TYPE_NAME."
