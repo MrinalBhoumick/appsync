dab9b0eae81a3033dd8663902e3ae4aba75153be
MrinalBhoumick <mrinalbhoumick0610@gmail.com>
2024-04-07 15:35:03 +0530

    updated response mapping template

--- scripts/update-response-mapping-template.sh
+++ scripts/update-response-mapping-template.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
+RESPONSE_TEMPLATE_FILE="response.vtl"
+
 # Create response.vtl file with the response mapping template content
 echo '#if( $context.result && $context.result.error )
     $util.error($context.result.error.message)
 #else
     $util.toJson($context.result)
-#end' > response.vtl
-
-RESPONSE_TEMPLATE_FILE="response.vtl"
+#end' > "$RESPONSE_TEMPLATE_FILE"
 
 # Fetch the schema definition
 SCHEMA=$(aws appsync get-introspection-schema --api-id "$API_ID" --format SDL)
@@ -17,6 +17,7 @@ RESOLVER_TYPE_NAME=$(echo "$SCHEMA" | grep -oP 'type \K\w+' | grep ResolverType)
 
 # List all resolvers of the resolver type
 RESOLVERS=$(aws appsync list-resolvers --api-id "$API_ID" --type-name "$RESOLVER_TYPE_NAME" --query "resolvers[*].[fieldName,typeName]" --output text)
+
 while IFS=$'\t' read -r RESOLVER_NAME TYPE_NAME; do
   if aws appsync update-resolver --api-id "$API_ID" --type-name "$TYPE_NAME" --field-name "$RESOLVER_NAME" --response-mapping-template "file://$RESPONSE_TEMPLATE_FILE"; then
     echo "Updated response mapping template for resolver $RESOLVER_NAME of type $TYPE_NAME."
