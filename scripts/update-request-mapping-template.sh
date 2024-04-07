2d1bab1362fe5d46840c47602a8e450fef19aeff
MrinalBhoumick <mrinalbhoumick0610@gmail.com>
2024-04-07 13:38:04 +0530

    updated request mapping template

--- scripts/update-request-mapping-template.sh
+++ scripts/update-request-mapping-template.sh
@@ -1,34 +1,25 @@
 #!/bin/bash
-
-# Ensure API_ID is defined
-if [ -z "$API_ID" ]; then
-  echo "API_ID must be defined."
-  exit 1
-fi
-
-# Set the request template file
-REQUEST_TEMPLATE_FILE="request.vt1"
+REQUEST_TEMPLATE_FILE="request.vtl"
 
 # List all types from the schema
 TYPES=$(aws appsync list-types --api-id "$API_ID")
 
-# Check if list-types command was successful
-if [ $? -ne 0 ]; then
-  echo "Failed to list types from the schema."
-  exit 1
-fi
+# Extract type names
+TYPE_NAMES=$(echo "$TYPES" | jq -r '.types[] | select(.definition | startswith("type")) | .name')
 
-# Extract resolver names and type names
-RESOLVER_NAMES=$(echo "$TYPES" | jq -r '.types[] | select(.definition | startswith("type ResolverType")) | .name')
-TYPE_NAMES=$(echo "$TYPES" | jq -r '.types[] | select(.definition | startswith("type ResolverType")) | .name')
-
-# Loop over resolver names and update request mapping template
-while read -r RESOLVER_NAME && read -r TYPE_NAME <&3; do
-  if aws appsync update-resolver --api-id "$API_ID" --type-name "$TYPE_NAME" --field-name "$RESOLVER_NAME" --request-mapping-template file://"$REQUEST_TEMPLATE_FILE"; then
-    echo "Updated request mapping template for resolver $RESOLVER_NAME of type $TYPE_NAME."
-  else
-    echo "Failed to update request mapping template for resolver $RESOLVER_NAME of type $TYPE_NAME."
-  fi
-done <<< "$RESOLVER_NAMES" 3<<< "$TYPE_NAMES"
+# Loop over type names and update request mapping template for each resolver
+for TYPE_NAME in $TYPE_NAMES; do
+  RESOLVERS=$(aws appsync list-resolvers --api-id "$API_ID" --type-name "$TYPE_NAME")
+  RESOLVER_NAMES=$(echo "$RESOLVERS" | jq -r '.resolvers[] | .fieldName')
+  
+  # Loop over resolver names and update request mapping template
+  while read -r RESOLVER_NAME; do
+    if aws appsync update-resolver --api-id "$API_ID" --type-name "$TYPE_NAME" --field-name "$RESOLVER_NAME" --request-mapping-template file://"$REQUEST_TEMPLATE_FILE"; then
+      echo "Updated request mapping template for resolver $RESOLVER_NAME of type $TYPE_NAME."
+    else
+      echo "Failed to update request mapping template for resolver $RESOLVER_NAME of type $TYPE_NAME."
+    fi
+  done <<< "$RESOLVER_NAMES"
+done
 
 echo "Completed updating request mapping templates."
