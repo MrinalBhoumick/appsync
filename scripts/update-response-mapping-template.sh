b26e870c6008cc7f2d0452cdf14efd39421def99
MrinalBhoumick <mrinalbhoumick0610@gmail.com>
2024-04-07 15:29:04 +0530

    updated response mapping template

--- scripts/update-response-mapping-template.sh
+++ scripts/update-response-mapping-template.sh
@@ -21,7 +21,7 @@ RESOLVER_NAMES=$(echo "$RESOLVERS" | jq -r '.resolvers[].fieldName')
 
 # Loop over resolver names and update response mapping template
 for RESOLVER_NAME in $RESOLVER_NAMES; do
-  if aws appsync update-resolver --api-id "$API_ID" --type-name "$RESOLVER_TYPE_NAME" --field-name "$RESOLVER_NAME" --response-mapping-template file://"$RESPONSE_TEMPLATE_FILE"; then
+  if aws appsync update-resolver --api-id "$API_ID" --type-name "$RESOLVER_TYPE_NAME" --field-name "$RESOLVER_NAME" --response-mapping-template "file://$RESPONSE_TEMPLATE_FILE"; then
     echo "Updated response mapping template for resolver $RESOLVER_NAME of type $RESOLVER_TYPE_NAME."
   else
     echo "Failed to update response mapping template for resolver $RESOLVER_NAME of type $RESOLVER_TYPE_NAME."
