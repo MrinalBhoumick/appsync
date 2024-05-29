17d0b0029455899e444a7295735884b46a094d9c
MrinalBhoumick <mrinalbhoumick0610@gmail.com>
2024-05-30 02:22:38 +0530

    updated scripts

--- scripts/start-schema.sh
+++ scripts/start-schema.sh
@@ -16,8 +16,16 @@ fi
 # Encode the GraphQL schema file in base64
 SCHEMA_BASE64=$(base64 -w 0 schema.graphql)
 
+# Update the schema for the AppSync API
+if aws appsync update-graphql-api --api-id "$API_ID" --name "$API_NAME" --authentication-type "$AUTHENTICATION_TYPE"; then
+    echo "API name and authentication type updated successfully."
+else
+    echo "Failed to update API name and authentication type."
+    exit 1
+fi
+
 # Initiate schema creation for the AppSync API
-if aws appsync update-graphql-api --api-id "$API_ID" --name "$API_NAME" --authentication-type "$AUTHENTICATION_TYPE" --definition "data:text/plain;base64,$SCHEMA_BASE64"; then
+if aws appsync start-schema-creation --api-id "$API_ID" --definition "data:text/plain;base64,$SCHEMA_BASE64"; then
     echo "Schema update initiated successfully."
 else
     echo "Failed to initiate schema update."
