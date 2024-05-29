1f6fd341c5b10d587fe7ff777ac0f758c0b2fbde
MrinalBhoumick <mrinalbhoumick0610@gmail.com>
2024-05-30 02:18:27 +0530

    updated scripts

--- scripts/start-schema.sh
+++ scripts/start-schema.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
 
-# Make sure API_ID is set before running this script
-# Set the path to the schema file
-SCHEMA_FILE="templates/data.graphql"
+# Make sure API_ID, API_NAME, and AUTHENTICATION_TYPE are set before running this script
+
 
 echo "Starting schema creation for AppSync API with API ID $API_ID"
 
-# Update the schema for the AppSync API
-if aws appsync update-graphql-api --api-id "$API_ID" --definition "file://$SCHEMA_FILE"; then
-    echo "Schema updated successfully."
+# Fetch the GraphQL schema file from GitHub
+if curl -o schema.graphql "$GITHUB_REPO_URL/templates/data.graphql"; then
+    echo "Schema file downloaded successfully."
 else
-    echo "Failed to update schema."
+    echo "Failed to download the schema file."
+    exit 1
+fi
+
+# Encode the GraphQL schema file in base64
+SCHEMA_BASE64=$(base64 -w 0 schema.graphql)
+
+# Initiate schema creation for the AppSync API
+if aws appsync update-graphql-api --api-id "$API_ID" --name "$API_NAME" --authentication-type "$AUTHENTICATION_TYPE" --definition "data:text/plain;base64,$SCHEMA_BASE64"; then
+    echo "Schema update initiated successfully."
+else
+    echo "Failed to initiate schema update."
     exit 1
 fi
 
