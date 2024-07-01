e087067fbe9b15e63ebcb1cae53f692da25f9756
MrinalBhoumick <mrinalbhoumick0610@gmail.com>
2024-07-01 15:15:35 +0530

    updated schemas

--- scripts/update-schema.sh
+++ scripts/update-schema.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-# Check if AWS CLI and Python are installed
-command -v aws >/dev/null 2>&1 || { echo >&2 "AWS CLI is not installed. Aborting."; exit 1; }
-command -v python3 >/dev/null 2>&1 || { echo >&2 "Python 3 is not installed. Aborting."; exit 1; }
-
-# Check if appsync-schema-uploader package is installed; install if necessary
-if ! python3 -m pip show appsync-schema-uploader >/dev/null 2>&1; then
-    echo "Installing appsync-schema-uploader..."
-    python3 -m pip install appsync-schema-uploader
-fi
-
 # Configure AWS CLI if not already configured
 if [[ ! -f ~/.aws/credentials ]]; then
     echo "AWS CLI is not configured. Configuring AWS CLI..."
