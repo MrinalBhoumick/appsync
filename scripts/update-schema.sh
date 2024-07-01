99ab3747c159fd02a10ca4f1f5a9ca3755dda5cc
MrinalBhoumick <mrinalbhoumick0610@gmail.com>
2024-07-01 14:26:37 +0530

    updated schemas

--- scripts/update-schema.sh
+++ scripts/update-schema.sh
@@ -2,12 +2,12 @@
 
 # Check if AWS CLI and Python are installed
 command -v aws >/dev/null 2>&1 || { echo >&2 "AWS CLI is not installed. Aborting."; exit 1; }
-command -v python >/dev/null 2>&1 || { echo >&2 "Python is not installed. Aborting."; exit 1; }
+command -v python3 >/dev/null 2>&1 || { echo >&2 "Python 3 is not installed. Aborting."; exit 1; }
 
 # Check if appsync-schema-uploader package is installed; install if necessary
-if ! python -m pip show appsync-schema-uploader >/dev/null 2>&1; then
+if ! python3 -m pip show appsync-schema-uploader >/dev/null 2>&1; then
     echo "Installing appsync-schema-uploader..."
-    python -m pip install appsync-schema-uploader
+    python3 -m pip install appsync-schema-uploader
 fi
 
 # Configure AWS CLI if not already configured
@@ -18,14 +18,14 @@ fi
 
 # Fetch AppSync API ID from CodeBuild environment
 if [[ -z "$API_ID" ]]; then
-    echo "Error: AWS_APPSYNC_API_ID environment variable is not set."
+    echo "Error: API_ID environment variable is not set."
     exit 1
 fi
 
-API_ID="$AWS_APPSYNC_API_ID"
+API_ID="$API_ID"
 
 # Determine the path to the templates directory relative to the script's location
-SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 TEMPLATES_DIR="$SCRIPT_DIR/../templates"
 SCHEMA_FILE="$TEMPLATES_DIR/data.graphql"
 
@@ -37,6 +37,6 @@ fi
 
 # Update the AppSync API schema using appsync-schema-uploader
 echo "Updating AppSync API schema..."
-python -m appsync_schema_uploader --api-id "$API_ID" --schema "$SCHEMA_FILE"
+python3 -m appsync_schema_uploader --api-id "$API_ID" --schema "$SCHEMA_FILE"
 
 echo "Schema update process completed."
